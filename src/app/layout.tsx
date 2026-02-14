ff6969e1ad55f55e3e2aaad1d9c1acbeb04b0059
sanjeevnc-claw <sanjeev.nc20@gmail.com>
2026-02-14 20:40:29 +0530

    Add src/app/layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,4 @@
+export const metadata = { title: 'cursor-web-app-e2a9' };
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return <html lang="en"><body>{children}</body></html>;
+}
